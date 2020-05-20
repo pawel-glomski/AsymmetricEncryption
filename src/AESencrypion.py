5c754e0a9735d16f5d5d681259f1141e4e7ec7c8
axontom <tomek.rusinowicz@gmail.com>
2020-05-20 21:51:59 +0200

    Header save/load functions

--- src/AESencrypion.py
+++ src/AESencrypion.py
@@ -2,18 +2,29 @@ import os
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
+def saveHeader(file, mode, filesize, IV):
+    file.write(str(len(mode)).zfill(16).encode('utf-8'))
+    file.write(mode.zfill(16).encode('utf-8'))
+    file.write(filesize.encode('utf-8'))
+    file.write(IV)
+
+def loadHeader(file):
+    modeLength = int(file.read(16))
+    mode = file.read(16).decode('utf-8')
+    mode = mode[-modeLength:]
+    filesize = int(file.read(16))
+    IV = file.read(16)
+    return mode, filesize, IV
+
 def AESencryptCBC(key, filename, outputFilename, chunkSize=64*1024):
     filesize = str(os.path.getsize(filename)).zfill(16)
     IV = get_random_bytes(16)
     encryptor = AES.new(key, AES.MODE_CBC, IV)
-    
+
     with open(filename, 'rb') as fi:
         with open(outputFilename, 'wb') as fo:
             mode = 'CBC'
-            fo.write(str(len(mode)).zfill(16).encode('utf-8'))
-            fo.write(mode.zfill(16).encode('utf-8'))
-            fo.write(filesize.encode('utf-8'))
-            fo.write(IV)
+            saveHeader(fo, mode, filesize, IV)
             while True:
                 chunk  = fi.read(chunkSize)
                 if len(chunk) == 0:
@@ -24,10 +35,7 @@ def AESencryptCBC(key, filename, outputFilename, chunkSize=64*1024):
 
 def AESdecryptCBC(key, filename, outputFilename, chunkSize=64*1024):
     with open(filename, 'rb') as fi:
-        modeLength = int(fi.read(16))
-        mode = fi.read(16).decode('utf-8')
-        filesize = int(fi.read(16))
-        IV = fi.read(16)
+        _, filesize, IV = loadHeader(fi)
         decryptor = AES.new(key, AES.MODE_CBC, IV)
         with open(outputFilename, 'wb') as fo:
             while True:
@@ -39,9 +47,8 @@ def AESdecryptCBC(key, filename, outputFilename, chunkSize=64*1024):
 
 def AESdecrypt(key, filename, outputFilename, chunkSize=64*1024):
     with open(filename, 'rb') as fi:
-        modeLength = int(fi.read(16))
-        mode = fi.read(16).decode('utf-8')
-    if mode[-modeLength:] == 'CBC':
+        mode, _, __ = loadHeader(fi)
+    if mode == 'CBC':
         AESdecryptCBC(key, filename, outputFilename, chunkSize=64*1024)
     else:
         print('Unknown mode: '+mode)
