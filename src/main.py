d7d24a8269b29c0f95dc66f2896d8fedf778547e
Exepp <pavel.glomski@gmail.com>
2020-05-20 11:59:46 +0200

    Basic user interface

--- src/main.py
+++ src/main.py
@@ -1,6 +1,31 @@
+
+from PyQt5 import uic
 from PyQt5.QtWidgets import *
 
+
+class UiWindow(QMainWindow):
+    def __init__(self):
+        super(UiWindow, self).__init__()
+        uic.loadUi('dialog.ui', self)
+
+        self.fileButton = self.findChild(QToolButton, 'fileButton')
+        self.inputPath = self.findChild(QLineEdit, 'inputPath')
+        self.outputPath = self.findChild(QLineEdit, 'outputPath')
+        self.encryptButton = self.findChild(QPushButton, 'encryptButton')
+        self.algorithmsBox = self.findChild(QComboBox, 'algorithmsBox')
+
+        self.fileButton.clicked.connect(self.chooseInputFile)
+        self.encryptButton.clicked.connect(self.encrypt)
+
+        self.show()
+
+    def chooseInputFile(self):
+        self.inputPath.setText(QFileDialog.getOpenFileName()[0])
+
+    def encrypt(self):
+        print("Szyfrowanie pliku:", self.inputPath.text(), "do:", self.outputPath.text(), "algorytmem: ", self.algorithmsBox.currentText())
+
+
 app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
+window = UiWindow()
 app.exec_()
